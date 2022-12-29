3d85b7eddd2cc8e5ab8dd5bbf5458aa2b5f4452a
christ-dos <christine_dos_santos@hotmail.fr>
2022-12-29 14:58:30 +0100

    implement getAllRecette dur le RecetteController

--- src/main/java/fr/dawan/formation/controller/RecetteController.java
+++ src/main/java/fr/dawan/formation/controller/RecetteController.java
@@ -1,9 +1,17 @@
 package fr.dawan.formation.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.dawan.formation.interfaces.IRecetteService;
+import fr.dawan.formation.model.Recette;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -11,5 +19,13 @@ import lombok.extern.slf4j.Slf4j;
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/recette")
 public class RecetteController {
+    @Autowired
+    private IRecetteService recetteService;
 
+    @GetMapping("/all")
+    public ResponseEntity<List<Recette>> getAllRecette() {
+        List<Recette> recettes = recetteService.findAll();
+        return new ResponseEntity<>(recettes, HttpStatus.OK);
+
+    }
 }
