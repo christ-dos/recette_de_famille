47042b5c9bb42e13fc540fd1123ad32ac86cd525
christ-dos <christine_dos_santos@hotmail.fr>
2022-12-30 18:45:11 +0100

    mise en place du controller des erreurs avec implementation pour IngredientNotFoundException et RecetteNotFoundException

--- src/main/java/fr/dawan/formation/controller/RecetteController.java
+++ src/main/java/fr/dawan/formation/controller/RecetteController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.dawan.formation.exception.RecetteNotFoundException;
 import fr.dawan.formation.interfaces.IRecetteService;
 import fr.dawan.formation.model.Recette;
 import lombok.extern.slf4j.Slf4j;
@@ -53,14 +52,7 @@ public class RecetteController {
 
     @PutMapping("/update")
     public ResponseEntity<?> updateRecette(@Valid @RequestBody Recette recette) {
-        Recette updateRecette = null;
-
-        try {
-            updateRecette = recetteService.updateRecette(recette);
-        } catch (RecetteNotFoundException ex) {
-            return new ResponseEntity<>(ex.getMessage(), HttpStatus.OK);
-        }
-
+        Recette updateRecette = recetteService.updateRecette(recette);
         log.debug("Controller: Recette mit à jour pour l'ID: " + updateRecette.getId());
         return new ResponseEntity<>(updateRecette, HttpStatus.OK);
     }
---
+++ src/main/java/fr/dawan/formation/exception/AdvisorErrorsController.java
@@ -0,0 +1,52 @@
+package fr.dawan.formation.exception;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class AdvisorErrorsController extends ResponseEntityExceptionHandler {
+
+//    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+//            HttpHeaders headers, HttpStatus status, WebRequest request) {
+//        Map<String, Object> body = new LinkedHashMap<>();
+//        body.put("timestamp", LocalDateTime.now());
+//        body.put("status", status.value());
+//
+//        List<String> errors = ex.getBindingResult().getFieldErrors().stream()
+//                .map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toList());
+//
+//        body.put("errors", errors);
+//
+//        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+//    }
+
+    @ExceptionHandler(RecetteNotFoundException.class)
+    public ResponseEntity<Object> handleRecetteNotFoundException(RecetteNotFoundException ex, WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IngredientNotFoundException.class)
+    public ResponseEntity<Object> handleIngredientNotFoundException(IngredientNotFoundException ex,
+            WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+}
